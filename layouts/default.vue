373ed2303386e8918241168c98e2335b94fbd7c0
a3318375 <184608371@qq.com>
2021-12-23 17:35:07 +0800

    11

--- layouts/default.vue
+++ layouts/default.vue
@@ -13,12 +13,43 @@ function handleScroll() {
 
 onMounted(async () => {
   document.addEventListener("scroll", handleScroll);
+  initAudio();
+  new l2dViewer({
+    el: document.getElementById('L2dCanvas'), // 要添加Live2d的元素, 支持dom选择器和jq选择器
+    basePath: 'https://www.inyaa.cn/lv2d/live2d/model', // 模型根目录
+    width: 800,
+    height: 600,
+    modelName: 'xuefeng_3', // 模型名称
+    sounds: [ // 触摸播放声音
+      'sounds/demo.mp3', // 相对路径是相对于模型文件夹
+      'https://cdn.jsdelivr.net/npm/live2dv3@latest/assets/biaoqiang_3/sounds/demo.mp3' // 也可以是网址
+    ]
+  })
 });
 //filter blur-sm
+function initAudio() {
+  // 创建一个音乐播放器实例，并挂载到DOM上，同时进行相关配置
+  const ap = new APlayer({
+    container: document.getElementById("aplayer"),
+    fixed: true,
+    audio: [ // 歌曲列表
+      {
+        name: "星茶会",
+        artist: "灰澈-星茶会",
+        url: "https://media.inyaa.cn/test_music.mp3",
+        cover: "http://imge.kugou.com/stdmusic/150/20200812/20200812134914113741.jpg",
+        lrc: "",
+        theme: "#baf",
+      },
+    ]
+  });
+}
 </script>
 
 <template>
   <img  @scroll="handleScroll" :class="[show ? 'fixed w-full h-full object-cover -z-999' : 'fixed filter blur-sm w-full h-full object-cover -z-999']" src="https://media.inyaa.cn/cover/7037ade43b1e484eac903a111b7ea709.jpg" >
+  <div id="aplayer"></div>
+  <div class="Canvas" style="position: fixed; opacity: 1; right: -300px; bottom: -100px; pointer-events: none;" id="L2dCanvas"></div>
   <div class="min-h-full">
     <InyaaMenu />
     <slot />
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,6 +11,17 @@ export default defineNuxtConfig({
   buildModules: [
     'nuxt-windicss',
   ],
+  meta: {
+    script: [
+      { src: 'https://cdn.jsdelivr.net/npm/aplayer@1.10.1/dist/APlayer.min.js' },
+      { src: 'https://cubism.live2d.com/sdk-web/cubismcore/live2dcubismcore.min.js' },
+      { src: 'https://cdn.jsdelivr.net/npm/pixi.js@4.6.1/dist/pixi.min.js' },
+      { src: 'https://cdn.jsdelivr.net/npm/live2dv3@1.2.2/live2dv3.min.js' },
+    ],
+    link: [
+      { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/aplayer@1.10.1/dist/APlayer.min.css' },
+    ],
+  },
   css: [
     '~/assets/github.css',
     '~/assets/sakura.css',
