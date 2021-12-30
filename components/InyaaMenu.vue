36461d5e47828e16df2fd4959e5091b63bec1d82
a3318375 <184608371@qq.com>
2021-12-30 17:50:21 +0800

    11

--- components/InyaaMenu.vue
+++ components/InyaaMenu.vue
@@ -49,16 +49,16 @@ function hideLoginMenu() {
 }
 
 const { show, updateShow, updateHide  } = useScroll();
-const { mobileMenuShow, updateMobileMenu } = useMobileMenuHide();
+const { mobileMenuShow, updateMobileMenu, updateMobileMenuHide } = useMobileMenuHide();
 </script>
 <template>
   <InyaaDialog />
-  <div :class="[ mobileMenuShow ? 'fixed pl-3 pt-3 z-888 md:hidden transition duration-300 transform translate-x-250px' : 'fixed pl-3 pt-3 z-888 md:hidden transition duration-300 transform translate-x-0']">
-    <div class="i-carbon-menu w-24px h-24px" @click="updateMobileMenu" v-if="!mobileMenuShow"/>
-    <div class="i-carbon-x w-24px h-24px" @click="updateMobileMenu" v-if="mobileMenuShow"/>
+  <div :class="[ mobileMenuShow ? 'fixed pl-3 pt-3 z-888 md:hidden transition duration-300 transform translate-x-250px' : 'fixed pl-3 pt-3 z-888 md:hidden transition duration-300 transform translate-x-0']" @click="updateMobileMenu">
+    <div class="i-carbon-menu w-24px h-24px" v-if="!mobileMenuShow"/>
+    <div class="i-carbon-x w-24px h-24px" v-if="mobileMenuShow"/>
   </div>
-  <div :class="[ mobileMenuShow ? 'fixed z-888 bg-white w-250px h-full transition duration-300 transform translate-x-0' : 'fixed z-888 bg-white w-250px h-full transition duration-300 transform -translate-x-250px']">
-    <div class="relative py-40px w-250px mx-75px text-center ">
+  <div :class="[ mobileMenuShow ? 'fixed z-777 bg-white w-250px h-full transition duration-300 transform translate-x-0' : 'fixed z-777 bg-white w-250px h-full transition duration-300 transform -translate-x-250px']">
+    <div class="py-40px w-250px mx-75px">
       <img class="rounded-full w-100px h-100px" src="https://media.inyaa.cn/99db72d3abaaef4beece7e9f94b3623.jpg" />
     </div>
     <ul>
