7c98de142d033cd0d7d14c0e677c51da1253bc29
a3318375 <184608371@qq.com>
2022-02-16 17:38:00 +0800

    cos

--- components/InyaaFooter.vue
+++ components/InyaaFooter.vue
@@ -14,9 +14,6 @@ const { data } = await useFetch('/config/findAll', {
     <footer class="text-white">
       <p><a href="http://www.beian.miit.gov.cn" target="_blank">{{data.data.WEB_ICP}}</a></p>
       <p>{{data.data.WEB_NAME}} ©2021 Created by {{data.data.WEB_ADMIN_NAME}}</p>
-      <p>本站由 <a href="https://www.upyun.com/?utm_source=lianmeng&amp;utm_medium=referral">
-        <img class="inline" src="https://media.inyaa.cn/upai.png!brand"></a> 提供CDN加速/云存储服务
-      </p>
     </footer>
   </div>
 </template>
