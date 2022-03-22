cbfb5386a3a2a3ac182276516a3a107e2cfa0d9e
a3318375 <184608371@qq.com>
2022-03-22 19:36:07 +0800

    修改readme

--- components/InyaaFooter.vue
+++ components/InyaaFooter.vue
@@ -6,15 +6,23 @@ const { data } = await useFetch('/config/findAll', {
   params: {
     type: 0
   }
+}).then(r => {
+  if (!r) {
+    return {};
+  }
+  if (r.code && r.code === 200) {
+    return r.data
+  } else {
+    return {};
+  }
 })
-console.log(111, data)
 </script>
 
 <template>
   <div class="pt-4 text-center">
     <footer class="text-white">
-      <p><a href="https://beian.miit.gov.cn" target="_blank">{{data.data.WEB_ICP}}</a></p>
-      <p>{{data.data.WEB_NAME}} ©2021 Created by {{data.data.WEB_ADMIN_NAME}}</p>
+      <p><a href="https://beian.miit.gov.cn" target="_blank">{{data.WEB_ICP}}</a></p>
+      <p>{{data.WEB_NAME}} ©2021 Created by {{data.WEB_ADMIN_NAME}}</p>
     </footer>
   </div>
 </template>
