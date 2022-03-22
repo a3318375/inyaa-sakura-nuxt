5a0a8a832ebc172659b353f73587e0b576a625d9
a3318375 <184608371@qq.com>
2022-03-22 20:33:07 +0800

    修改readme

--- components/InyaaFooter.vue
+++ components/InyaaFooter.vue
@@ -22,8 +22,8 @@ console.log(123, data)
 <template>
   <div class="pt-4 text-center">
     <footer class="text-white">
-      <p><a href="https://beian.miit.gov.cn" target="_blank">{{data.WEB_ICP}}</a></p>
-      <p>{{data.WEB_NAME}} ©2021 Created by {{data.WEB_ADMIN_NAME}}</p>
+      <p><a href="https://beian.miit.gov.cn" target="_blank">{{data?.WEB_ICP}}</a></p>
+      <p>{{data?.WEB_NAME}} ©2021 Created by {{data?.WEB_ADMIN_NAME}}</p>
     </footer>
   </div>
 </template>
